a1ae6c0637fea0c90955a6012fe294a8b1116f67
limkl.ryan@gmail.com <limkl.ryan@gmail.com>
2025-02-14 14:58:29 +0800

    updated .gitignore

--- LearningAPI/Migrations/20250214064443_initialcreate.Designer.cs
+++ LearningAPI/Migrations/20250214064443_initialcreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LearningAPI.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    [Migration("20250206073436_initialcreate")]
+    [Migration("20250214064443_initialcreate")]
     partial class initialcreate
     {
         /// <inheritdoc />
